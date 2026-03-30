847e282e16b0d0d33835d0a73fea76feaaf58137
Yassin-Aka <yassin.akachar@viacesi.fr>
2026-03-30 08:59:42 +0200

    mise a jour de base de donnée

--- stagepro.fr/stagepro.sql
+++ stagepro.fr/stagepro.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 27 mars 2026 à 15:21
+-- Généré le : lun. 30 mars 2026 à 08:59
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -42,7 +42,7 @@ CREATE TABLE `candidatures` (
 --
 
 INSERT INTO `candidatures` (`id`, `utilisateur_id`, `offre_id`, `cv`, `lettre_motivation`, `statut`, `created_at`) VALUES
-(4, 3, 3, 'uploads/1774620928_Dossier_Synthese_Valentin_PLEINDOUX_CPI A2 Info 25-26 Aix_Semestre_3 (22).PDF', 'eded', 'en_attente', '2026-03-27 14:15:28');
+(5, 3, 2, 'uploads/1774621442_Dossier_Synthese_Yassin_AKACHAR_CPI A2 Info 25-26 Aix_Semestre_4 (2).PDF', 'ihi', 'en_attente', '2026-03-27 14:24:02');
 
 -- --------------------------------------------------------
 
@@ -64,8 +64,11 @@ CREATE TABLE `entreprises` (
 --
 
 INSERT INTO `entreprises` (`id`, `nom`, `description`, `email_contact`, `telephone_contact`, `created_at`) VALUES
-(2, 'DataFlow', 'Entreprise spécialisée en data', 'contact@dataflow.fr', '0607080910', '2026-03-24 13:45:29'),
-(3, 'CreativeLab', 'Agence UX/UI et design produit', 'contact@creativelab.fr', '0708091011', '2026-03-24 13:45:29');
+(2, 'DataFlow', 'DataFlow est une entreprise spécialisée en data science et intelligence artificielle. Elle développe des solutions d’analyse de données avancées pour aider les entreprises à prendre des décisions stratégiques.', 'contact@dataflow.fr', '0607080910', '2026-03-24 13:45:29'),
+(3, 'CreativeLab', 'CreativeLab est une agence digitale spécialisée dans le design UX/UI et la création d’expériences numériques innovantes. Elle accompagne ses clients dans la conception d’interfaces modernes, intuitives et performantes.', 'contact@creativelab.fr', '0708091011', '2026-03-24 13:45:29'),
+(6, 'TechCorp', 'TechCorp est une entreprise spécialisée dans le développement de solutions web et logicielles sur mesure. Elle conçoit des plateformes digitales performantes adaptées aux besoins des entreprises.', 'contact@techcorp.fr', '0611223344', '2026-03-27 14:27:50'),
+(7, 'SecureIT', 'SecureIT est une entreprise spécialisée en cybersécurité et protection des systèmes informatiques. Elle accompagne les entreprises dans la sécurisation de leurs infrastructures.', 'contact@secureit.fr', '0677889900', '2026-03-27 14:27:50'),
+(8, 'CyberNova', 'CyberNova est une entreprise innovante spécialisée dans la cybersécurité et l’intelligence artificielle. Elle développe des solutions de protection avancées pour les entreprises.', 'contact@cybernova.fr', '0678451239', '2026-03-27 14:27:50');
 
 -- --------------------------------------------------------
 
@@ -93,7 +96,20 @@ CREATE TABLE `offres` (
 
 INSERT INTO `offres` (`id`, `entreprise_id`, `titre`, `description`, `competences`, `localite`, `duree`, `nb_places`, `remuneration`, `date_offre`, `created_at`) VALUES
 (2, 2, 'Data Analyst', 'Analyse de données et reporting', NULL, NULL, NULL, 1, 900.00, '2026-03-21', '2026-03-24 13:45:29'),
-(3, 3, 'UI/UX Designer', 'Conception d’interfaces et expérience utilisateur', '', 'Paris', '6', 1, 152000.00, '2026-03-22', '2026-03-24 13:45:29');
+(15, 3, 'UX/UI Designer Junior', 'Création de maquettes et amélioration UX.', NULL, NULL, NULL, 1, 800.00, '2026-03-27', '2026-03-27 14:31:40'),
+(16, 2, 'Data Analyst', 'Analyse de données et dashboards.', NULL, NULL, NULL, 1, 900.00, '2026-03-27', '2026-03-27 14:31:40'),
+(17, 6, 'Développeur Web Fullstack', 'Développement web PHP / JS.', NULL, NULL, NULL, 1, 850.00, '2026-03-27', '2026-03-27 14:31:40'),
+(18, 7, 'Consultant Cybersécurité', 'Audit et sécurité des systèmes.', NULL, NULL, NULL, 1, 950.00, '2026-03-27', '2026-03-27 14:31:40'),
+(19, 8, 'Ingénieur IA & Sécurité', 'Détection d’intrusions avec IA.', NULL, NULL, NULL, 1, 1000.00, '2026-03-27', '2026-03-27 14:31:40'),
+(20, 6, 'Développeur Mobile', 'Applications Android/iOS.', NULL, NULL, NULL, 1, 800.00, '2026-03-27', '2026-03-27 14:31:40'),
+(21, 3, 'Marketing Digital', 'SEO et réseaux sociaux.', NULL, NULL, NULL, 1, 700.00, '2026-03-27', '2026-03-27 14:31:40'),
+(22, 3, 'UX/UI Designer Junior', 'Création de maquettes et amélioration UX.', NULL, NULL, NULL, 1, 800.00, '2026-03-27', '2026-03-27 14:34:11'),
+(23, 2, 'Data Analyst', 'Analyse de données et dashboards.', NULL, NULL, NULL, 1, 900.00, '2026-03-27', '2026-03-27 14:34:11'),
+(24, 6, 'Développeur Web Fullstack', 'Développement web PHP / JS.', NULL, NULL, NULL, 1, 850.00, '2026-03-27', '2026-03-27 14:34:11'),
+(25, 7, 'Consultant Cybersécurité', 'Audit et sécurité des systèmes.', NULL, NULL, NULL, 1, 950.00, '2026-03-27', '2026-03-27 14:34:11'),
+(26, 8, 'Ingénieur IA & Sécurité', 'Détection d’intrusions avec IA.', NULL, NULL, NULL, 1, 1000.00, '2026-03-27', '2026-03-27 14:34:11'),
+(27, 6, 'Développeur Mobile', 'Applications Android/iOS.', NULL, NULL, NULL, 1, 800.00, '2026-03-27', '2026-03-27 14:34:11'),
+(28, 3, 'Marketing Digital', 'SEO et réseaux sociaux.', NULL, NULL, NULL, 1, 700.00, '2026-03-27', '2026-03-27 14:34:11');
 
 -- --------------------------------------------------------
 
@@ -157,13 +173,6 @@ CREATE TABLE `wishlist` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Déchargement des données de la table `wishlist`
---
-
-INSERT INTO `wishlist` (`id`, `utilisateur_id`, `offre_id`, `created_at`) VALUES
-(6, 2, 3, '2026-03-25 07:52:25');
-
---
 -- Index pour les tables déchargées
 --
 
@@ -219,19 +228,19 @@ ALTER TABLE `wishlist`
 -- AUTO_INCREMENT pour la table `candidatures`
 --
 ALTER TABLE `candidatures`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `entreprises`
 --
 ALTER TABLE `entreprises`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT pour la table `offres`
 --
 ALTER TABLE `offres`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT pour la table `roles`
